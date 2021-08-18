f6767d369f4a5140848b0553430d71bccbb98074
Trevor McGarrah <86972467+McRavenBerry@users.noreply.github.com>
2021-08-18 12:16:20 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+let mySprite = sprites.create(img`
+    . . . . . . . . c c c c . . . . 
+    . . . . . . c c d d d d c . . . 
+    . . . . . c c c c c c d c . . . 
+    . . . . c c 4 4 4 4 d c c . c c 
+    . . . c 4 d 4 4 4 4 4 1 c c 4 c 
+    . . c 4 4 4 1 4 4 4 4 d 1 c 4 f 
+    . c 4 4 4 4 1 4 4 4 4 4 1 4 4 f 
+    f 4 4 4 4 4 1 1 c f 4 4 1 f 4 f 
+    f 4 4 4 f 4 1 c 4 f 4 4 1 f 4 f 
+    f 4 4 4 4 4 1 4 4 f 4 4 d f f f 
+    . f 4 4 4 4 1 c c 4 4 d f f . . 
+    . . f f 4 d 4 4 4 4 4 c f . . . 
+    . . . . f f 4 4 4 4 c d b c . . 
+    . . . . . . f f f f d d d c . . 
+    . . . . . . . . . . c c c . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite)
+scene.setBackgroundColor(8)
+music.playMelody("- C G - A - E C5 ", 120)
